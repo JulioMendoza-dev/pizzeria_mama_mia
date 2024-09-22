e5a85e803b6af82f05984119375ea33e27c36ec9
julio <juliofacundo2018@hotmail.com>
2024-09-21 22:57:17 -0300

    completando header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,15 @@ function Header() {
  
     return (
     <>
-      <div className="titulo-comentario text-center pb-3">
-        <h1>___Pizzeria Mamma Mia___</h1>
+      <div className="titulo-comentario text-center pb-3" 
+      style={{
+       width:"100%",
+       height:"300px",
+       backgroundImage:`url(/pizza-header-2.jpg)`,
+       backgroundSize:'cover'
+       }}>
+
+        <h1>___Pizzeria Mamma Mia___</h1>   
         <br />
         <p>¡Tenemos las mejores pizzas que puedes encontrar!</p>
       </div>
@@ -13,3 +20,7 @@ function Header() {
 }
 
 export default Header
+
+
+
+
